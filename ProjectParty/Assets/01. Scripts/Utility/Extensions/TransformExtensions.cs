661017bf7bd1719e927c5526b3ae447a646b0314
SEH00N <06psh0501@gmail.com>
2024-04-05 22:50:53 +0900

    extensions

---
+++ ProjectParty/Assets/01. Scripts/Utility/Extensions/TransformExtensions.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public static class TransformExtensions
+{
+	public static bool InnerDistance(this Transform left, Transform right, float distance)
+    {
+        float sqrDistance = (left.position - right.position).sqrMagnitude;
+        return sqrDistance < (distance * distance);
+    }
+}
