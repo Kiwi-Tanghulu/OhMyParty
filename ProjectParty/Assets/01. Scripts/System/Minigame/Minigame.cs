021d227132ab6d19d8bfa3366501c1c37a3462f8
SEH00N <06psh0501@gmail.com>
2024-04-06 17:27:21 +0900

    asd

--- ProjectParty/Assets/01. Scripts/System/Minigame/Minigame.cs
+++ ProjectParty/Assets/01. Scripts/System/Minigame/Minigame.cs
@@ -1,12 +1,9 @@
 using Unity.Netcode;
-using UnityEngine;
 
 namespace OMG.Minigames
 {
     public abstract class Minigame : NetworkBehaviour
     {
-        // public NetworkObject NetworkObject { get; } = null;
-
         public abstract void StartGame();
         public abstract void FinishGame();
     }
