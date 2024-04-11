869f89697f7bd1c57822bfe7686af1b4ec112671
JUNSUNG06 <forest2847@gmail.com>
2024-04-11 10:12:09 +0900

    .

--- ProjectParty/Assets/01. Scripts/Test/TSofa.cs
+++ ProjectParty/Assets/01. Scripts/Test/TSofa.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using OMG.Interacting;
+using Unity.Netcode;
+using OMG.Players;
 
-public class TSofa : MonoBehaviour
+public class TSofa : MonoBehaviour, IInteractable
 {
-    
+    public bool Interact(Component performer, bool actived, Vector3 point = default)
+    {
+        ActioningPlayer actioningPlayer = performer.GetComponent<ActioningPlayer>();
+
+        return true;
+    }
 }
