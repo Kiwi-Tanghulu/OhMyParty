2470a3400fd51ddc56a93d3bf3264b402272cf54
SEH00N <06psh0501@gmail.com>
2024-04-23 12:29:51 +0900

    facepunch network

--- ProjectParty/Assets/01. Scripts/UI/3D/SolidUI.cs
+++ ProjectParty/Assets/01. Scripts/UI/3D/SolidUI.cs
@@ -4,6 +4,7 @@ namespace OMG.UI.Solid
 {
     public abstract class SolidUI : MonoBehaviour
     {
-        
+        [field : SerializeField]
+        public bool Active { get; protected set; } = true;
     }
 }
