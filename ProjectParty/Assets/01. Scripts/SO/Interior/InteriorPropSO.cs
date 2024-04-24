3089ba0cc22cd5982d6d5a59c6bc477fe3d5e851
SEH00N <06psh0501@gmail.com>
2024-04-24 19:47:29 +0900

    interior prop bound

---
+++ ProjectParty/Assets/01. Scripts/SO/Interior/InteriorPropSO.cs
@@ -0,0 +1,40 @@
+using OMG.Extensions;
+using UnityEngine;
+
+namespace OMG.Interiors
+{
+    [CreateAssetMenu(menuName = "SO/Interior/PropData")]
+    public class InteriorPropSO : ScriptableObject
+    {
+        public Mesh PropMesh = null;
+        public Vector3Int PropSize = Vector3Int.one;
+
+        [SerializeField] Vector3Int offset = Vector3Int.zero;
+        public Vector3Int Offset {
+            get => offset;
+            set {
+                offset = value.GetClampEach(-(PropSize - Vector3Int.one), Vector3Int.zero);
+            }
+        }
+
+        public Vector3 Pivot => offset + (Vector3)(PropSize - Vector3Int.one) * 0.5f;
+
+        private void OnValidate()
+        {
+            Offset = offset;
+        }
+
+        [SerializeField] bool drawGizmos = false;
+        public void DrawGizmos(Vector3 position, float gridSize)
+        {
+            if(drawGizmos == false)
+                return;
+
+            Gizmos.color = Color.red;
+            Vector3 size = (Vector3)PropSize * gridSize;
+            Vector3 center = position + Pivot * gridSize;
+
+            Gizmos.DrawWireCube(center, size);
+        }
+    }
+}
--- ProjectParty/Assets/01. Scripts/Utility/Extensions/VectorExtensions.cs
+++ ProjectParty/Assets/01. Scripts/Utility/Extensions/VectorExtensions.cs
@@ -30,6 +30,41 @@ namespace OMG.Extensions
             return value;
         }
 
+        #region Clamp
+        public static Vector3 GetClampEach(this Vector3 left, Vector3 min, Vector3 max)
+        {
+            if (min.x > max.x)
+                (min.x, max.x) = (max.x, min.x);
+            if (min.y > max.y)
+                (min.y, max.y) = (max.y, min.y);
+            if (min.z > max.z)
+                (min.z, max.z) = (max.z, min.z);
+
+            Vector3 value = Vector3.zero;
+            value.x = Mathf.Clamp(left.x, min.x, max.x);
+            value.y = Mathf.Clamp(left.y, min.y, max.y);
+            value.z = Mathf.Clamp(left.z, min.z, max.z);
+            return value;
+        }
+
+        public static Vector3Int GetClampEach(this Vector3Int left, Vector3Int min, Vector3Int max)
+        {
+            if(min.x > max.x)
+                (min.x, max.x) = (max.x, min.x);
+            if(min.y > max.y)
+                (min.y, max.y) = (max.y, min.y);
+            if(min.z > max.z)
+                (min.z, max.z) = (max.z, min.z);
+
+            Vector3Int value = Vector3Int.zero;
+            value.x = Mathf.Clamp(left.x, min.x, max.x);
+            value.y = Mathf.Clamp(left.y, min.y, max.y);
+            value.z = Mathf.Clamp(left.z, min.z, max.z);
+            return value;
+        }
+        #endregion
+
+        #region Abs
         public static void Abs(this Vector3 left)
         {
             left.x = Mathf.Abs(left.x);
@@ -46,5 +81,23 @@ namespace OMG.Extensions
             
             return value;
         }
+
+        public static void Abs(this Vector3Int left)
+        {
+            left.x = Mathf.Abs(left.x);
+            left.y = Mathf.Abs(left.y);
+            left.z = Mathf.Abs(left.z);
+        }
+
+        public static Vector3Int GetAbs(this Vector3Int left)
+        {
+            Vector3Int value = Vector3Int.zero;
+            value.x = Mathf.Abs(left.x);
+            value.y = Mathf.Abs(left.y);
+            value.z = Mathf.Abs(left.z);
+            
+            return value;
+        }
+        #endregion
     }
 }
