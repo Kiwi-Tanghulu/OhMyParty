6540c82c276943c7c8fe31824f11fca90a4f8af0
SEH00N <06psh0501@gmail.com>
2024-04-27 17:20:52 +0900

    grid shader

--- ProjectParty/Assets/01. Scripts/Test/TInterior.cs
+++ ProjectParty/Assets/01. Scripts/Test/TInterior.cs
@@ -18,35 +18,35 @@ namespace OMG.Test
             InputManager.ChangeInputMap(InputMapType.Interior);
         }
 
-        #if UNITY_EDITOR
-        private void OnDrawGizmos()
-        {
-            if(system == null)
-                return;
+        // #if UNITY_EDITOR
+        // private void OnDrawGizmos()
+        // {
+        //     if(system == null)
+        //         return;
 
-            int count = 100;
-            Gizmos.color = Color.black;
-            {
-                Vector3 startPosition = new Vector3(-100, 0, 0);
-                Vector3 endPosition = new Vector3(100, 0, 0);
-                for (int i = -count / 2; i < count / 2; ++ i)
-                {
-                    endPosition.z = i * system.GridSize;
-                    startPosition.z = i * system.GridSize;
-                    Gizmos.DrawLine(startPosition, endPosition);
-                }
-            }
-            {
-                Vector3 startPosition = new Vector3(0, 0, -100);
-                Vector3 endPosition = new Vector3(0, 0, 100);
-                for (int i = -count / 2; i < count / 2; ++ i)
-                {
-                    endPosition.x = i * system.GridSize;
-                    startPosition.x = i * system.GridSize;
-                    Gizmos.DrawLine(startPosition, endPosition);
-                }
-            }
-        }
-        #endif
+        //     int count = 100;
+        //     Gizmos.color = Color.black;
+        //     {
+        //         Vector3 startPosition = new Vector3(-100, 0, 0);
+        //         Vector3 endPosition = new Vector3(100, 0, 0);
+        //         for (int i = -count / 2; i < count / 2; ++ i)
+        //         {
+        //             endPosition.z = i * system.GridSize;
+        //             startPosition.z = i * system.GridSize;
+        //             Gizmos.DrawLine(startPosition, endPosition);
+        //         }
+        //     }
+        //     {
+        //         Vector3 startPosition = new Vector3(0, 0, -100);
+        //         Vector3 endPosition = new Vector3(0, 0, 100);
+        //         for (int i = -count / 2; i < count / 2; ++ i)
+        //         {
+        //             endPosition.x = i * system.GridSize;
+        //             startPosition.x = i * system.GridSize;
+        //             Gizmos.DrawLine(startPosition, endPosition);
+        //         }
+        //     }
+        // }
+        // #endif
     }
 }
