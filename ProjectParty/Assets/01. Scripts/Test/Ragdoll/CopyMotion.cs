fce00d3e7df82eca3bb9f7f452140010ecd82e4b
JUNSUNG06 <forest2847@gmail.com>
2024-05-06 18:26:53 +0900

    copy motion

--- ProjectParty/Assets/01. Scripts/Test/Ragdoll/CopyMotion.cs
+++ ProjectParty/Assets/01. Scripts/Test/Ragdoll/CopyMotion.cs
@@ -8,6 +8,9 @@ public class CopyMotion : MonoBehaviour
     public bool IsRagdoll;
     private ConfigurableJoint joint;
 
+    private Vector3 MirrorAnchorPosition;
+    private Quaternion MirrorAnchorRotation;
+
     private void Awake()
     {
         joint = GetComponent<ConfigurableJoint>();
@@ -17,13 +20,33 @@ public class CopyMotion : MonoBehaviour
     {
         if (gameObject.name != CopyTrm.name)
             Debug.Log(123);
+
+        MirrorAnchorRotation = CopyTrm.rotation;
     }
 
     private void Update()
     {
-        if (IsRagdoll)
-            joint.targetRotation = CopyTrm.rotation;
+        if(IsRagdoll)
+        {
+            Vector3 MirrorTargetPosition = GetTargetPosition(CopyTrm.transform.position, MirrorAnchorPosition);
+            joint.targetPosition = MirrorTargetPosition;
+
+            Quaternion MirrorTargetRotation = GetTargetRotation(CopyTrm.transform.rotation, MirrorAnchorRotation);
+            joint.targetRotation = MirrorTargetRotation;
+        }
         else
+        {
             transform.localRotation = CopyTrm.localRotation;
+        }
+    }
+
+    Vector3 GetTargetPosition(Vector3 currentPosition, Vector3 anchorPosition)
+    {
+        return anchorPosition - currentPosition;
+    }
+
+    Quaternion GetTargetRotation(Quaternion currentRotation, Quaternion anchorRotation)
+    {
+        return Quaternion.Inverse(currentRotation) * anchorRotation;
     }
 }
