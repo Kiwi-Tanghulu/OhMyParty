6d9eccee6af33a3ec48779c6a0a780c96b5c9818
SEH00N <06psh0501@gmail.com>
2024-05-20 19:13:35 +0900

    session name setting by steam nickname

--- ProjectParty/Assets/01. Scripts/Core/HostManager.cs
+++ ProjectParty/Assets/01. Scripts/Core/HostManager.cs
@@ -39,7 +39,7 @@ namespace OMG.Network
 
             ClientManager.Instance.CurrentLobby = await SteamMatchmaking.CreateLobbyAsync(maxMember);
             ClientManager.Instance.CurrentLobby?.SetData("private", "false");
-            ClientManager.Instance.CurrentLobby?.SetData("owner", "SEH00N");
+            ClientManager.Instance.CurrentLobby?.SetData("owner", SteamClient.Name);
             onHostStarted?.Invoke();
         }
 
