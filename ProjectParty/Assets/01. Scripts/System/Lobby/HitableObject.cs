a1b442bd8eea5838b6e8c4f2b5a86266928cfe3b
JUNSUNG06 <forest2847@gmail.com>
2024-06-04 19:46:36 +0900

    top render object

--- ProjectParty/Assets/01. Scripts/System/Lobby/HitableObject.cs
+++ ProjectParty/Assets/01. Scripts/System/Lobby/HitableObject.cs
@@ -5,22 +5,10 @@ using UnityEngine.Events;
 
 public class HitableObject : MonoBehaviour, IDamageable
 {
-    [SerializeField] private Transform damagedTransform;
-    public UnityEvent<Transform/*DamagedTransform*/> OnDamagedEvent;
-
-    private void Start()
-    {
-        if(damagedTransform == null)
-            damagedTransform = transform;
-    }
-
-    public Transform GetDamagedTransfrom()
-    {
-        return damagedTransform;
-    }
+    public UnityEvent<Vector3/*hit point*/> OnDamagedEvent;
 
     public void OnDamaged(float damage, Transform attacker, Vector3 point, Vector3 normal = default)
     {
-        OnDamagedEvent?.Invoke(GetDamagedTransfrom());
+        OnDamagedEvent?.Invoke(point);
     }
 }
