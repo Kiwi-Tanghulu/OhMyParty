baa23601bbf57cccea73d99d2c88d576390d653e
JUNSUNG06 <forest2847@gmail.com>
2024-06-06 22:52:59 +0900

    audio manager

---
+++ ProjectParty/Assets/01. Scripts/Core/AudioManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace OMG
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager Instance { get; private set; }
+
+        [SerializeField] private AudioMixer audioMixer;
+
+        private void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+            else
+                Destroy(gameObject);
+        }
+
+        public void SetVolume(AudioGroupType groupType, float volume)
+        {
+            audioMixer.SetFloat(groupType.ToString(), volume * 100f - 80f);
+        }
+    }
+}
