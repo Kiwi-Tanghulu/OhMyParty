d71bdb3c54c12c7bb213beda9be4fc442eb9ef52
SEH00N <06psh0501@gmail.com>
2024-06-08 22:52:01 +0900

    tile collision fix

--- ProjectParty/Assets/01. Scripts/System/Feedback/PlayParticleFeedback.cs
+++ ProjectParty/Assets/01. Scripts/System/Feedback/PlayParticleFeedback.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using OMG.Extensions;
 using UnityEngine;
 
 namespace OMG.Feedbacks
 {
     public class PlayParticleFeedback : Feedback
     {
+        [SerializeField] float delay = 0.6f;
         private List<ParticleSystem> particles = null;
 
         private void Awake()
@@ -15,7 +17,7 @@ namespace OMG.Feedbacks
 
         public override void Play(Vector3 playPos)
         {
-            particles.ForEach(i => i.Play());
+            StartCoroutine(this.DelayCoroutine(delay, () => particles.ForEach(i => i.Play())));
         }
     }
 }
