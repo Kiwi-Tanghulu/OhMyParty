e6e9ce5a33ce576c515eed522467053866b50c43
SEH00N <06psh0501@gmail.com>
2024-06-09 00:08:44 +0900

    asd

--- ProjectParty/Assets/01. Scripts/System/Minigame/SafetyZone/Item/FishItem.cs
+++ ProjectParty/Assets/01. Scripts/System/Minigame/SafetyZone/Item/FishItem.cs
@@ -9,6 +9,9 @@ namespace OMG.Minigames.SafetyZone
 
         public override void OnCollision(Collision other)
         {
+            if(false) // 부딪힌 것이 본인 클라라면
+                return;
+
             if(other.transform.TryGetComponent<IDamageable>(out IDamageable id))
                 id?.OnDamaged(power, transform, other.contacts[0].point, other.contacts[0].normal);
 
--- ProjectParty/Assets/01. Scripts/System/Minigame/SafetyZone/Item/FruitItem.cs
+++ ProjectParty/Assets/01. Scripts/System/Minigame/SafetyZone/Item/FruitItem.cs
@@ -8,6 +8,9 @@ namespace OMG.Minigames.SafetyZone
 
         public override void OnCollision(Collision other)
         {
+            if(false) // 부딪힌 것이 본인 클라라면
+                return;
+                
             if(other.transform.TryGetComponent<IDamageable>(out IDamageable id))
                 id?.OnDamaged(power, transform, other.contacts[0].point, other.contacts[0].normal);
 
