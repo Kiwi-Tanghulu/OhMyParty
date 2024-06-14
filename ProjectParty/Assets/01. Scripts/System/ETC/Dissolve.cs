df1af3dfc3665f36c19d5b810affe306b10b6d5f
JUNSUNG06 <forest2847@gmail.com>
2024-06-15 00:56:51 +0900

    dissolve

---
+++ ProjectParty/Assets/01. Scripts/System/ETC/Dissolve.cs
@@ -0,0 +1,37 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OMG
+{
+    public class Dissolve : MonoBehaviour
+    {
+        private Material mat;
+        private readonly string propertyID = "_Dissolve_Value";
+
+        private void Start()
+        {
+            Renderer render = GetComponent<Renderer>();
+            Material sharedMat = render.sharedMaterial;
+            mat = new Material(sharedMat);
+            render.material = mat;
+        }
+
+        public void ShowDissolve(float time)
+        {
+            DODissolve(0f, 1.2f, time);
+        }
+
+        public void HideDissolve(float time)
+        {
+            DODissolve(1.2f, 0f, time);
+        }
+
+        public void DODissolve(float start, float end, float time)
+        {
+            mat.SetFloat(propertyID, start);
+            mat.DOFloat(end, propertyID, time);
+        }
+    }
+}
