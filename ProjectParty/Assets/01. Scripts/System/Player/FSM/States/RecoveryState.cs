1c028c88f95338e4b0c4d0d3bd70c5191664b91f
JUNSUNG06 <forest2847@gmail.com>
2024-06-17 00:21:29 +0900

    player anim

--- ProjectParty/Assets/01. Scripts/System/Player/FSM/States/RecoveryState.cs
+++ ProjectParty/Assets/01. Scripts/System/Player/FSM/States/RecoveryState.cs
@@ -11,41 +11,6 @@ namespace OMG.Player.FSM
 {
     public class RecoveryState : PlayerFSMState
     {
-        [SerializeField] private LayerMask groundLayer;
 
-        private CharacterMovement movement;
-        private ExtendedAnimator anim;
-        private RagdollController ragdoll;
-
-        private readonly int recoveryDirHash = Animator.StringToHash("recoveryDir");
-        private readonly int recoveryHash = Animator.StringToHash("recovery");
-        private readonly string recoverySelectAnim = "RecoverySelect";
-
-        public override void InitState(FSMBrain brain)
-        {
-            base.InitState(brain);
-
-            movement = player.GetComponent<CharacterMovement>();
-            anim = player.Animator;
-            ragdoll = player.Visual.Ragdoll;
-        }
-
-        protected override void OwnerEnterState()
-        {
-            player.Visual.Ragdoll.SetActive(false);
-
-            RaycastHit[] hit = Physics.RaycastAll(ragdoll.HipRb.transform.position + Vector3.up * 1000f, Vector3.down, 10000f, groundLayer);
-            if(hit.Length > 0)
-            {
-                movement.Teleport(hit[0].point, transform.rotation);
-            }
-
-            //anim.PlayAnim(recoverySelectAnim, AnimatorLayerType.Default);
-
-            int recoDir = ragdoll.HipRb.transform.forward.y > 0f ? 1 : -1;
-            anim.SetInt(recoveryDirHash, recoDir);
-
-            base.OwnerEnterState();
-        }
     }
 }
