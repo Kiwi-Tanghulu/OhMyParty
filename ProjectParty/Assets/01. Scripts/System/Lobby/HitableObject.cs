e79e8b4c3eb8c43d41c23d8d7bf8e67dab4a36fe
JUNSUNG06 <forest2847@gmail.com>
2024-06-16 22:39:43 +0900

    .

--- ProjectParty/Assets/01. Scripts/System/Lobby/HitableObject.cs
+++ ProjectParty/Assets/01. Scripts/System/Lobby/HitableObject.cs
@@ -2,18 +2,18 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class HitableObject : NetworkBehaviour, IDamageable
+public class HitableObject : MonoBehaviour, IDamageable
 {
     public UnityEvent<Vector3/*hit point*/> OnDamagedEvent;
 
     public void OnDamaged(float damage, Transform attacker, Vector3 point, Vector3 normal = default)
     {
-        OnDamagedClientRpc(point);
+        //OnDamagedClientRpc(point);
     }
 
-    [ClientRpc]
-    private void OnDamagedClientRpc(Vector3 point)
-    {
-        OnDamagedEvent?.Invoke(point);
-    }
+    //[ClientRpc]
+    //private void OnDamagedClientRpc(Vector3 point)
+    //{
+    //    OnDamagedEvent?.Invoke(point);
+    //}
 }
