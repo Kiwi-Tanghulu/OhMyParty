b695c7b118f838992fbb1ba5375f10a142740a88
SEH00N <06psh0501@gmail.com>
2024-07-01 22:50:44 +0900

    CollectionExtensions_Suffle

--- ProjectParty/Assets/01. Scripts/Utility/Extensions/CollectionExtensions.cs
+++ ProjectParty/Assets/01. Scripts/Utility/Extensions/CollectionExtensions.cs
@@ -29,15 +29,11 @@ namespace OMG.Extensions
 
         public static List<T> Shuffle<T>(this List<T> source)
         {
-            List<T> result = new List<T>();
-            for(int i = 0; i < source.Count; i++)
-            {
-                result.Add(source[i]);
-            }
+            List<T> result = new List<T>(source);
 
             for(int i = 0; i < result.Count; i++)
             {
-                int index = Random.Range(0, source.Count);
+                int index = Random.Range(i, source.Count);
                 T temp = result[i];
 
                 result[i] = result[index];
