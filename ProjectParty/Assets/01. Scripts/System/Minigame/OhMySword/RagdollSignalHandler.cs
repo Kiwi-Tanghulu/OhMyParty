4bc72f73a7a281bcd93a354486f0f06816177cbf
kwak1s1h <kwak1s1h@kakao.com>
2024-08-07 06:11:30 +0900

    OhMySword Timeline Sound

---
+++ ProjectParty/Assets/01. Scripts/System/Minigame/OhMySword/RagdollSignalHandler.cs
@@ -0,0 +1,32 @@
+using OMG.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollSignalHandler : MonoBehaviour
+{
+	[SerializeField] private List<PlayerRagdollController> _ragdollControllers;
+	[SerializeField] private float _force = 5f;
+
+	private int _currentIdx = 0;
+
+	private void Awake()
+	{
+		_currentIdx = 0;
+	}
+
+	public void HandleSignal()
+	{
+		Debug.Log("Ragdoll Signal");
+
+		PlayerRagdollController controller = _ragdollControllers[_currentIdx];
+
+		controller.SetActive(true);
+		controller.GetComponent<Animator>().enabled = false;
+		controller.transform.parent.GetComponent<Animator>().enabled = false;
+
+		controller.AddForce(_force, -controller.transform.forward, ForceMode.Impulse);
+
+		_currentIdx++;
+	}
+}
