1595ca228921b73eb060687a98cb1c1cfca673af
SEH00N <06psh0501@gmail.com>
2024-09-09 12:23:30 +0900

    soccer physics

---
+++ ProjectParty/Assets/01. Scripts/System/Minigame/WhatTheFC/SoccerBall.cs
@@ -0,0 +1,53 @@
+using OMG.NetworkEvents;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace OMG.Minigames.WhatTheFC
+{
+    public class SoccerBall : NetworkBehaviour, IDamageable
+    {
+        [SerializeField] float shootSpeed = 10f;
+        [SerializeField] UnityEvent<Vector3> onHitEvent = null;
+
+        private NetworkEvent<Vector3Params, Vector3> onHitNetworkEvent = new NetworkEvent<Vector3Params, Vector3>("BallHit");
+
+        private Rigidbody ballRigidbody = null;
+
+        private void Awake()
+        {
+            ballRigidbody = GetComponent<Rigidbody>();
+        }
+
+        public override void OnNetworkSpawn()
+        {
+            base.OnNetworkSpawn();
+
+            onHitNetworkEvent.AddListener(HandleHit);
+            onHitNetworkEvent.Register(NetworkObject);
+        }
+
+        public override void OnNetworkDespawn()
+        {
+            base.OnNetworkDespawn();
+
+            onHitNetworkEvent.Unregister();
+        }
+
+        public void OnDamaged(float damage, Transform attacker, Vector3 point, HitEffectType effectType, Vector3 normal = default, Vector3 direction = default)
+        {
+            onHitNetworkEvent?.Broadcast(point, false);
+        }
+
+        private void HandleHit(Vector3 hitPosition)
+        {
+            onHitEvent?.Invoke(hitPosition);
+            
+            if(IsHost)
+            {
+                Vector3 direction = (transform.position - hitPosition).normalized;
+                ballRigidbody.AddForce(direction * shootSpeed, ForceMode.Impulse);
+            }
+        }
+    }
+}
--- ProjectParty/Assets/01. Scripts/System/Minigame/WhatTheFC/WhatTheFC.cs
+++ ProjectParty/Assets/01. Scripts/System/Minigame/WhatTheFC/WhatTheFC.cs
@@ -1,13 +1,46 @@
+using Cinemachine;
+using OMG.Networks;
+using OMG.Utility;
 using UnityEngine;
 
 namespace OMG.Minigames.WhatTheFC
 {
     public class WhatTheFC : PlayableMinigame
     {
+        [SerializeField] OptOption<CinemachineVirtualCamera> minigameVCamOption = null;
+
+        [Space(10f)]
+        [SerializeField] SoccerBall ballPrefab = null;
+        [SerializeField] Transform ballPosition = null;
+        private SoccerBall ball = null;
+
         protected override void OnGameInit()
         {
             base.OnGameInit();
             (cycle as TeamMinigameCycle).DecideTeam();
+        }
+
+        protected override void OnGameStart()
+        {
+            base.OnGameStart();
+
+            if(IsHost)
+            {
+                ball = Instantiate(ballPrefab, ballPosition.position, Quaternion.identity);
+                ball.NetworkObject.Spawn();
+            }
+
+            bool teamFlag = (cycle as TeamMinigameCycle).TeamInfo[ClientManager.Instance.ClientID];
+            minigameVCamOption[teamFlag].Priority = DEFINE.FOCUSED_PRIORITY;
+            minigameVCamOption[!teamFlag].Priority = DEFINE.UNFOCUSED_PRIORITY;
+        }
+
+        protected override void OnGameRelease()
+        {
+            base.OnGameRelease();
+
+            if(IsHost)
+                ball.NetworkObject.Despawn();
         }
     }
 }
