ecbb450e51b3c85c8065f7b9733b0049f4a4e85f
JUNSUNG06 <forest2847@gmail.com>
2024-09-09 15:21:49 +0900

    gametext

---
+++ ProjectParty/Assets/01. Scripts/UI/Minigame/Base/GameText.cs
@@ -0,0 +1,60 @@
+using DG.Tweening;
+using OMG.Minigames;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using static OMG.UI.GameCycleText;
+
+namespace OMG.UI
+{
+    public class GameText : MonoBehaviour
+    {
+        [SerializeField] private float startReadyTextSize;
+        [SerializeField] private float endReadyTextSize;
+
+        [Space]
+        [SerializeField] private float showTime;
+        [SerializeField] private float hideDelayTime;
+        [SerializeField] private float hideTime;
+
+        private TextMeshProUGUI text;
+
+        private Sequence seq;
+
+        public UnityEvent OnPlayEvent;
+        public UnityEvent OnShowEvent;
+        public UnityEvent OnEndEvent;
+
+        private void Start()
+        {
+            text = GetComponent<TextMeshProUGUI>(); 
+
+            seq = DOTween.Sequence();
+            seq.SetAutoKill(false);
+
+            seq.AppendCallback(() => text.gameObject.SetActive(true));
+            seq.AppendCallback(() => OnPlayEvent?.Invoke());
+            seq.Append(text.transform.DOScale(Vector3.one * endReadyTextSize, showTime)
+                .From(Vector3.one * startReadyTextSize));
+            seq.Join(text.DOFade(1f, showTime)
+                .From(0f));
+            seq.AppendCallback(() => OnShowEvent?.Invoke());
+            seq.AppendInterval(hideDelayTime);
+            seq.Append(text.DOFade(0f, hideTime));
+            seq.AppendCallback(() => OnEndEvent?.Invoke());
+            seq.AppendCallback(() => text.gameObject.SetActive(false));
+
+            text.gameObject.SetActive(false);
+
+            Play();
+        }
+
+        public void Play()
+        {
+            seq.Restart();
+        }
+    }
+}
